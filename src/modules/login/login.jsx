fc29652349519996fada0d246e2b42f6b37aaf4f
David RL <alejandro20rodriguez04@gmail.com>
2021-06-09 21:57:20 -0700

    adding global variables fonts importing Login main design structure

--- src/modules/login/login.jsx
+++ src/modules/login/login.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
-import img from '../../assets/login-piñata.png'
+import logo_piñata from '../../assets/login-piñata.png'
+import logo from '../../assets/logo.png'
 import './Login.scss'
 
 const Login = () => {
     return (
         <div>
             <div className='login-image__container'>
-                <img src={img} alt="" />
+                <img src={logo_piñata} alt="" />
             </div>
-            <div className='form-container'>
-                <div className="form-container__subcontainer">
-                    hello world
+            <div className='form'>
+                <div className="form__subcontainer">
+                    <div className='image-wrapper'>
+                        <img src={logo} alt="Party2Go logo" />
+                    </div>
+                    <form action="">
+                        <label htmlFor="email">Correo Electronico</label>
+                        <input name="email" type="text" placeholder="test@gmail.com" />
+                        <label htmlFor="password">Contraseña</label>
+                        <input name="password" type="password" placeholder="****" />
+                        
+                        <div className='additional-info'>
+                            <span className='error'>Contraseña o correo incorrecto</span>
+                            <a className='forgot-password' href="/forgot-password"><span>Olvide mi contraseña</span></a>
+                        </div>
+                        <div className='button-wrapper'>
+                            <button className='login-button'>Ingresar</button>
+                        </div>
+                    </form>
                 </div>
-                <span>¿No tienes una cuenta? <a href="">Crea una</a></span>
+                <span>¿No tienes una cuenta? <a href="/register">Crea una</a></span>
             </div>
         </div>
     )
