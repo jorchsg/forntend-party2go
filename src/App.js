90d79b46c242e0d1046699eed58aa933b7fbabf1
David RL <alejandro20rodriguez04@gmail.com>
2021-06-14 20:24:47 -0700

    Added default layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Login from './modules/login/login';
 import Home from '../src/modules/home/Home';
 import { Switch, Route, } from "react-router-dom";
+import DefaultLayout from './modules/DefaultLayout';
 import './index.scss';
 
 function App() {
   return (
     <Switch>
-      <Route path="/" exact component={Home}></Route>
       <Route path="/login" exact component={Login} />
+      <DefaultLayout>
+        <Route path="/" exact component={Home}></Route>
+      </DefaultLayout>
     </Switch>
   );
 }
