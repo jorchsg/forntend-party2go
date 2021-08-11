517d5d1f15b7e12ca6899daff48f1e0420cb3eaf
David RL <alejandro20rodriguez04@gmail.com>
2021-08-10 20:23:35 -0700

    redirects to home after approved transaction

--- src/modules/pago/index.jsx
+++ src/modules/pago/index.jsx
@@ -7,16 +7,16 @@ import "../pago/style.scss";
 import { UserContext } from "../../context/UserProvider";
 import { AppContext } from "../../components/Provider";
 import jsPDF from "jspdf";
-
+import { useHistory } from "react-router-dom";
 
 const Pago = () => {
   const [paymentSelected, setPaymentSelected] = useState(undefined);
   // const [editingInfo, setEditingInfo] = useState(false);
   // const [editingAddr, setEditingAddr] = useState(false);
-  const [context]=React.useContext(AppContext);
-  const [user]=React.useContext(UserContext);
+  const [context] = React.useContext(AppContext);
+  const [user] = React.useContext(UserContext);
+  const history = useHistory();
 
- 
   const getTotal = () => {
     let total = 0;
     //Salon
@@ -25,90 +25,171 @@ const Pago = () => {
     }
     //Mesas
     if (context.mesaSelected) {
-      total = total + parseFloat(context.mesaSelected.precioUnitario * context.mesasQuantity);
+      total =
+        total +
+        parseFloat(context.mesaSelected.precioUnitario * context.mesasQuantity);
     }
     //Mesas
     if (context.sillaSelected) {
-      total = total + parseFloat(context.sillaSelected.precioUnitario * context.sillasQuantity);
+      total =
+        total +
+        parseFloat(
+          context.sillaSelected.precioUnitario * context.sillasQuantity
+        );
     }
     //Sonido
     if (context.sonidoSelected) {
       total = total + parseFloat(context.sonidoSelected.precioHora);
     }
-    return total
+    return total;
   };
 
-  
-  
-  function generateRecibe  () {
-    const pagoTotal=getTotal();
-    console.log(pagoTotal)
-    if (paymentSelected=="oxxo"){
-      const doc =jsPDF('p','pt');
-
-      doc.text(170,80,'Recibo de pago con referencia OXXO')
-      doc.setFont('courier');
-
-      //set the font type
-   
-      doc.addImage("https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Oxxo_Logo.svg/1200px-Oxxo_Logo.svg.png",200,90,200,100)
-
-      doc.text(120,250,'Nombre: '+ user.nombre +" "+ user.apellidoPaterno+" "+user.apellidoMaterno)
-      doc.text(120,270,'Datos de compra')
-      doc.text(120,290,!context.salonSelected?('Salon: '+" "+"$"+"0"):('Salon: '+" "+"$"+context.salonSelected.precio))
-      doc.text(120,310,!context.mesaSelected?('Mesas: '+" "+"$"+"0"):('Mesas: '+" "+"$"+(context.mesaSelected.precioUnitario * context.mesasQuantity)))
-      doc.text(120,330,!context.sillaSelected?('Sillas: '+"$"+"0"):('Sillas: '+"$"+(context.sillaSelected.precioUnitario * context.sillasQuantity)))
-      doc.text(120,350,!context.sonidoSelected?('Sonido: '+"$"+"0"):('Sonido: '+"$"+context.sonidoSelected.precioHora))
-      doc.text(120,365,"--------------------------------------")
-      doc.text(350,380,"Total: "+"$"+pagoTotal)
-      doc.text(120,420,"Numero de referencia de pago:")
-      doc.text(120,440,"13213543510313121321")
-
-      
-
-
-
-
-      
-      
-      doc.save("referenciaOxxo.pdf")
-    }
-    else if (paymentSelected=="paypal"){
-      const doc =jsPDF('p','pt');
-
-      doc.text(170,80,'Recibo de pago con referencia Paypal')
-      doc.setFont('courier');
-
-      //set the font type
-   
-      doc.addImage("https://www.actualidadecommerce.com/wp-content/uploads/2020/10/paypal.png",200,90,200,100)
-
-      doc.text(120,250,'Nombre: '+ user.nombre +" "+ user.apellidoPaterno+" "+user.apellidoMaterno)
-      doc.text(120,270,'Datos de compra')
-      doc.text(120,290,!context.salonSelected?('Salon: '+" "+"$"+"0"):('Salon: '+" "+"$"+context.salonSelected.precio))
-      doc.text(120,310,!context.mesaSelected?('Mesas: '+" "+"$"+"0"):('Mesas: '+" "+"$"+(context.mesaSelected.precioUnitario * context.mesasQuantity)))
-      doc.text(120,330,!context.sillaSelected?('Sillas: '+"$"+"0"):('Sillas: '+"$"+(context.sillaSelected.precioUnitario * context.sillasQuantity)))
-      doc.text(120,350,!context.sonidoSelected?('Sonido: '+"$"+"0"):('Sonido: '+"$"+context.sonidoSelected.precioHora))
-      doc.text(120,365,"--------------------------------------")
-      doc.text(350,380,"Total: "+"$"+pagoTotal)
-      doc.text(120,420,"Numero de referencia de pago:")
-      doc.text(120,440,"13213543510313121321")
-
-      
-
-
-
-
-      
-      
-      doc.save("referenciaPaypal.pdf")
-    }else if(paymentSelected==undefined){
-      alert("You need to select a Payment Method")
+  function generateRecibe() {
+    const pagoTotal = getTotal();
+    if (!paymentSelected) {
+      alert("You need to select a Payment Method");
+      return;
     }
 
+    if (paymentSelected == "oxxo") {
+      const doc = jsPDF("p", "pt");
 
+      doc.text(170, 80, "Recibo de pago con referencia OXXO");
+      doc.setFont("courier");
+
+      //set the font type
+
+      doc.addImage(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Oxxo_Logo.svg/1200px-Oxxo_Logo.svg.png",
+        200,
+        90,
+        200,
+        100
+      );
+
+      doc.text(
+        120,
+        250,
+        "Nombre: " +
+          user.nombre +
+          " " +
+          user.apellidoPaterno +
+          " " +
+          user.apellidoMaterno
+      );
+      doc.text(120, 270, "Datos de compra");
+      doc.text(
+        120,
+        290,
+        !context.salonSelected
+          ? "Salon: " + " " + "$" + "0"
+          : "Salon: " + " " + "$" + context.salonSelected.precio
+      );
+      doc.text(
+        120,
+        310,
+        !context.mesaSelected
+          ? "Mesas: " + " " + "$" + "0"
+          : "Mesas: " +
+              " " +
+              "$" +
+              context.mesaSelected.precioUnitario * context.mesasQuantity
+      );
+      doc.text(
+        120,
+        330,
+        !context.sillaSelected
+          ? "Sillas: " + "$" + "0"
+          : "Sillas: " +
+              "$" +
+              context.sillaSelected.precioUnitario * context.sillasQuantity
+      );
+      doc.text(
+        120,
+        350,
+        !context.sonidoSelected
+          ? "Sonido: " + "$" + "0"
+          : "Sonido: " + "$" + context.sonidoSelected.precioHora
+      );
+      doc.text(120, 365, "--------------------------------------");
+      doc.text(350, 380, "Total: " + "$" + pagoTotal);
+      doc.text(120, 420, "Numero de referencia de pago:");
+      doc.text(120, 440, "13213543510313121321");
+
+      doc.save("referenciaOxxo.pdf");
+    } else if (paymentSelected == "paypal") {
+      const doc = jsPDF("p", "pt");
+
+      doc.text(170, 80, "Recibo de pago con referencia Paypal");
+      doc.setFont("courier");
+
+      //set the font type
+
+      doc.addImage(
+        "https://www.actualidadecommerce.com/wp-content/uploads/2020/10/paypal.png",
+        200,
+        90,
+        200,
+        100
+      );
+
+      doc.text(
+        120,
+        250,
+        "Nombre: " +
+          user.nombre +
+          " " +
+          user.apellidoPaterno +
+          " " +
+          user.apellidoMaterno
+      );
+      doc.text(120, 270, "Datos de compra");
+      doc.text(
+        120,
+        290,
+        !context.salonSelected
+          ? "Salon: " + " " + "$" + "0"
+          : "Salon: " + " " + "$" + context.salonSelected.precio
+      );
+      doc.text(
+        120,
+        310,
+        !context.mesaSelected
+          ? "Mesas: " + " " + "$" + "0"
+          : "Mesas: " +
+              " " +
+              "$" +
+              context.mesaSelected.precioUnitario * context.mesasQuantity
+      );
+      doc.text(
+        120,
+        330,
+        !context.sillaSelected
+          ? "Sillas: " + "$" + "0"
+          : "Sillas: " +
+              "$" +
+              context.sillaSelected.precioUnitario * context.sillasQuantity
+      );
+      doc.text(
+        120,
+        350,
+        !context.sonidoSelected
+          ? "Sonido: " + "$" + "0"
+          : "Sonido: " + "$" + context.sonidoSelected.precioHora
+      );
+      doc.text(120, 365, "--------------------------------------");
+      doc.text(350, 380, "Total: " + "$" + pagoTotal);
+      doc.text(120, 420, "Numero de referencia de pago:");
+      doc.text(120, 440, "13213543510313121321");
+
+      doc.save("referenciaPaypal.pdf");
+    }
+
+    setTimeout(() => {
+      history.push("/");
+    }, 5000);
   }
-  
+
   return (
     <>
       <div className="pago-container">
@@ -142,7 +223,13 @@ const Pago = () => {
             <h3 className="title">Confirma tus datos</h3>
             <div className="data">
               <div className="data-info">
-                <p>{user.nombre +" "+ user.apellidoPaterno+" "+user.apellidoMaterno}</p>
+                <p>
+                  {user.nombre +
+                    " " +
+                    user.apellidoPaterno +
+                    " " +
+                    user.apellidoMaterno}
+                </p>
                 <p>{user.telefono}</p>
                 <p>{user.correo}</p>
               </div>
@@ -155,16 +242,33 @@ const Pago = () => {
             <h3 className="title">Direccion del Evento</h3>
             <div className="direction">
               <div className="direction-info">
-                {!context.salonSelected ?
-
-                <p>
-                  {user.calle+" "+ user.numInterior+","+" "+user.numExterior+","+" "+user.colonia+","+" "+ user.cp +","+" "+ user.ciudad+","+" "+user.estado+","+" "+"México"}
-                </p>
-                :
-                <p>
-                  {context.salonSelected.direccionSalon}
-                </p>
-                }
+                {!context.salonSelected ? (
+                  <p>
+                    {user.calle +
+                      " " +
+                      user.numInterior +
+                      "," +
+                      " " +
+                      user.numExterior +
+                      "," +
+                      " " +
+                      user.colonia +
+                      "," +
+                      " " +
+                      user.cp +
+                      "," +
+                      " " +
+                      user.ciudad +
+                      "," +
+                      " " +
+                      user.estado +
+                      "," +
+                      " " +
+                      "México"}
+                  </p>
+                ) : (
+                  <p>{context.salonSelected.direccionSalon}</p>
+                )}
               </div>
               <div className="editar">
                 <a href="#Edit">Editar</a>
@@ -183,46 +287,49 @@ const Pago = () => {
             <div className="list">
               <div className="concepto">
                 <p>Salon</p>
-                {
-                  !context.salonSelected ?
-                  <p>{"$"+"0"}</p>
-                  :
-                  <p>{"$"+context.salonSelected.precio}</p>
-                }
-                
+                {!context.salonSelected ? (
+                  <p>{"$" + "0"}</p>
+                ) : (
+                  <p>{"$" + context.salonSelected.precio}</p>
+                )}
               </div>
               <div className="concepto">
                 <p>Mesas</p>
-                {
-                  !context.mesaSelected ?
-                  <p>{"$"+"0"}</p>
-                  :
-                  <p>{"$"+(context.mesaSelected.precioUnitario * context.mesasQuantity)}</p>
-                }
+                {!context.mesaSelected ? (
+                  <p>{"$" + "0"}</p>
+                ) : (
+                  <p>
+                    {"$" +
+                      context.mesaSelected.precioUnitario *
+                        context.mesasQuantity}
+                  </p>
+                )}
               </div>
               <div className="concepto">
                 <p>Sillas</p>
-                {
-                  !context.sillaSelected ?
-                  <p>{"$"+"0"}</p>
-                  :
-                  <p>{"$"+(context.sillaSelected.precioUnitario * context.sillasQuantity)}</p>
-                }
+                {!context.sillaSelected ? (
+                  <p>{"$" + "0"}</p>
+                ) : (
+                  <p>
+                    {"$" +
+                      context.sillaSelected.precioUnitario *
+                        context.sillasQuantity}
+                  </p>
+                )}
               </div>
               <div className="concepto">
                 <p>Sonido</p>
-                {
-                  !context.sonidoSelected ?
-                  <p>{"$"+"0"}</p>
-                  :
-                  <p>{"$"+context.sonidoSelected.precioHora}</p>
-                }
+                {!context.sonidoSelected ? (
+                  <p>{"$" + "0"}</p>
+                ) : (
+                  <p>{"$" + context.sonidoSelected.precioHora}</p>
+                )}
               </div>
             </div>
           </div>
           <div className="conceptoTotal">
             <h5>Total</h5>
-            <h4>{"$"+getTotal()}</h4>
+            <h4>{"$" + getTotal()}</h4>
           </div>
           <div className="confirm-button">
             <Button onClick={generateRecibe}>Confirmar solicitud</Button>
